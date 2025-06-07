b44bf717dcdebd1ff7852640aadde15791562c86
KevinTorres01 <kevintorresperera@gmail.com>
2025-06-07 19:38:44 -0400

    Ports checker

--- main.c
+++ main.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include "procces_info.c"
+#include "Ports Checker.c"
 
+int main()
+{
 
-int main() {
-    
-    PROCESS_INFO* p_process_table =create_procces_table();
+    PROCESS_INFO *p_process_table = create_procces_table();
     for (int i = 0; i < MAX_PROCESS_TABLE_LENGTH; i++)
     {
         print_info(&p_process_table[i]);
     }
     delete_process_table(p_process_table);
-
+    check_ports();
 }
